fd5065d45debafa25ebc96c8ecb7ce094d5f1e16
Ayush Joshi <ayush854032@gmail.com>
2022-06-22 15:55:08 +0530

    Add python bindings for `Random` library to access methods and class from `Python` code
    Signed-off-by: Ayush Joshi <ayush854032@gmail.com>

---
+++ aim/random/random.cc
@@ -0,0 +1,31 @@
+// Copyright 2022 The Aim Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "random/random.hh"
+
+using aim::random::BiggestInt;
+
+aim::random::Random::Random()
+    : generator_(std::random_device()()), rfrom_(kMinInt), rto_(kMaxInt) {}
+
+aim::random::Random::Random(const BiggestInt rto)
+    : generator_(std::random_device()()), rfrom_(kMinInt), rto_(rto) {}
+
+aim::random::Random::Random(const BiggestInt rfrom, const BiggestInt rto)
+    : generator_(std::random_device()()), rfrom_(rfrom), rto_(rto) {}
+
+BiggestInt aim::random::Random::Generate() {
+  std::uniform_int_distribution<BiggestInt> distr_(rfrom_, rto_);
+  return distr_(generator_);
+}
---
+++ include/random/random.hh
@@ -0,0 +1,52 @@
+// Copyright 2022 The Aim Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef AIM_INCLUDE_RANDOM_RANDOM_HH_
+#define AIM_INCLUDE_RANDOM_RANDOM_HH_
+
+#include <cstdint>
+#include <random>
+
+namespace aim {
+namespace random {
+using BiggestInt = std::int64_t;
+
+class RandomAbstractBaseClass {
+ public:
+  virtual ~RandomAbstractBaseClass() = default;
+  virtual BiggestInt Generate() = 0;
+};
+
+class Random : public RandomAbstractBaseClass {
+ public:
+  Random();
+
+  Random(const BiggestInt rto);
+
+  Random(const BiggestInt rfrom, const BiggestInt rto);
+
+  BiggestInt Generate() override;
+
+ private:
+  static constexpr BiggestInt kMaxInt = std::numeric_limits<BiggestInt>::max();
+  static constexpr BiggestInt kMinInt = std::numeric_limits<BiggestInt>::min();
+
+  BiggestInt rfrom_;
+  BiggestInt rto_;
+  std::mt19937 generator_;
+};
+}  // namespace random
+}  // namespace aim
+
+#endif  // AIM_INCLUDE_RANDOM_RANDOM_HH_
